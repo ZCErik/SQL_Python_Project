f32613b91aad9c3fc84a793343f1c775ce67874d
ZCErik <erikgsilva8@gmail.com>
2019-06-06 22:35:52 -0400

    sending email

--- emailSender.py
+++ emailSender.py
@@ -10,35 +10,35 @@ from test import *
 # print(today)
 print("Starting app...")
 def connect():
-    # smtp_server = "smtp.gmail.com"
-    # port = 587  # For starttls
+    smtp_server = "smtp.gmail.com"
+    port = 587  # For starttls
     sender_email = "erikgsilva8@gmail.com"
     password = input("Type your password for " + sender_email + " to send email: ")
+
+    # Create a secure SSL context
+    thisContext = ssl.create_default_context()
+
     emp = getEmpId()
     receiver_email = getEmpEmail(emp)
     employee = getEmpName(emp)
-    week = datetime.date(2019, 5, 22).isocalendar()[1]
+    week = datetime.date(2019, 6, 6).isocalendar()[1]
     message = 'Subject: {}\n\n{}'.format("Schedule for week: " + str(week), """\
     Hi """ + employee + """
         Here is your schedule for the """ + str(week) + """ 
     This message is sent from Python.""")
     SECURITY_EMAIL_SENDER = 'erikgsilva8@gmail.com'
     
-    # Create a secure SSL context
-    thisContext = ssl.create_default_context()
+    
      
     # Try to log in to server and send email
     try:
         # Secure the connection
         # TODO: Send email here
-        with smtplib.SMTP() as server:
-            server.connect('smtp.gmail.com', '587')
-            server.ehlo()  # Can be omitted 
-            server.starttls(context=thisContext)
-            server.ehlo()  # Can be omitted
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message)
-    
+        server = smtplib.SMTP(smtp_server,port)
+        server.ehlo() # Can be omitted
+        server.starttls(context=thisContext) # Secure the connection
+        server.ehlo() # Can be omitted
+        server.login(sender_email, password)
 
     except Exception as e:
         # Print any error messages to stdout
